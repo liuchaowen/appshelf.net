d4734b86b8fe8a0323c5b479b91e08c3208345b7
theshy <theshy@gitlab.com>
2026-02-05 09:57:44 +0800

    fix sth

---
+++ _next/static/V8z4RYgXIGbyhDIdz6vMW/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Farticle\u002F[title]\u002F[id]","\u002Fcategories\u002F[slug]","\u002Fcategories\u002F[slug]\u002F[id]","\u002Fdetails\u002F[pkg]\u002F[id]","\u002Fhot\u002F[slug]","\u002Fhot\u002F[slug]\u002F[id]","\u002Ftopics\u002F[title]\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
